b198db82d9003e9960fc834fd5d67a9d9e12228d
seece <pekka.vaananen@iki.fi>
2013-02-06 00:49:58 +0200

    add sound callback

--- addqd/sound.h
+++ addqd/sound.h
@@ -14,7 +14,7 @@
 
 #include "config.h"
 
-#define CHECK_ERROR if (result != MMSYSERR_NOERROR) { fprintf(stderr, "Couldn't open audio device. Error code: 0x%X\n", result); }
+#define CHECK_ERROR if (result != MMSYSERR_NOERROR) { fprintf(stderr, "Audio error at %s. Error code: 0x%X\n", __LINE__, result); }
 
 SAMPLE_TYPE	lpSoundBuffer[SYN_BUFFERSIZE*2]; 
 HWAVEOUT	hWaveOut;
@@ -53,13 +53,54 @@ struct AudioBank {
 
 struct AudioBank buffer[AUDIO_BANKS];
 
+double tt = 0.0;
+
 void render_sound(SAMPLE_TYPE * lpSoundBuffer) {
 	for (int i=0;i<SYN_BUFFERSIZE;i++) {
-		double t = i/(double)SYN_RATE;
-		lpSoundBuffer[i*2] = (sin(2.0*3.14*t*400.0) * 0.4) * 1.0;
+		double t = tt + i/(double)SYN_RATE;
+		lpSoundBuffer[i*2] = (sin(2.0*3.14*t*400.0) * 0.4) *( 1.0+sin(2.0*3.14*t*0.2)*0.5);
 		lpSoundBuffer[i*2+1] = lpSoundBuffer[i*2];
 	}
+
+	tt += SYN_BUFFERSIZE/(double)SYN_RATE;
 }
+
+
+static void CALLBACK fillSoundBuffer(HWAVEOUT hWaveOut,
+									 UINT uMsg,
+									 DWORD dwInstance,
+									 DWORD dwParam1,
+									 DWORD dwParam2 ) {
+
+	WAVEHDR* waveheader = (WAVEHDR*) dwParam1;
+
+	switch(uMsg) {
+		case WOM_DONE:
+			MMRESULT result; 
+			waveOutUnprepareHeader(hWaveOut, waveheader, sizeof(WaveHDR));
+
+			render_sound((float *)waveheader->lpData);
+
+			waveheader->dwFlags = 0;
+			result = waveOutPrepareHeader(hWaveOut, waveheader, sizeof(WaveHDR));
+			CHECK_ERROR;
+			waveOutWrite(hWaveOut,waveheader,sizeof(WAVEHDR));
+			CHECK_ERROR;
+
+			fprintf(stdout, "WOM_DONE\n");
+			break;
+		case WOM_OPEN:
+			fprintf(stdout, "WOM_OPEN\n");
+			break;
+		case WOM_CLOSE:
+			fprintf(stdout, "WOM_CLOSE\n");
+			break;
+		default:
+			fprintf(stderr, "Illegal audio callback.\n");
+			break;
+	}
+}
+
 
 void init_sound(void) {
 	/*
@@ -77,7 +118,7 @@ void init_sound(void) {
 	}
 
 	MMRESULT result; 
-	result = waveOutOpen(&hWaveOut, WAVE_MAPPER, &WaveFMT, NULL, 0, CALLBACK_NULL); 
+	result = waveOutOpen(&hWaveOut, WAVE_MAPPER, &WaveFMT, (DWORD_PTR)fillSoundBuffer, (DWORD)&WaveHDR, CALLBACK_FUNCTION); 
 	CHECK_ERROR;
 	result = waveOutPrepareHeader(hWaveOut, &WaveHDR, sizeof(WaveHDR));
 	CHECK_ERROR;
