7942f1e690549675ffe5ececcc6db56f74d7c552
Pekka Väänänen <pekka.vaananen@gmail.com>
2013-10-03 18:48:19 +0300

    expand unit documentation

--- addplayer/player/unit.h
+++ addplayer/player/unit.h
@@ -7,7 +7,9 @@ class CUnit : CDevice {
 	public:
 		CUnit();
 		~CUnit();
+		/// Renders a single sample with the given phase. Units can track time internally.
 		virtual float render(double phase) = 0;
+		/// Each Unit should have a function that kills all audio output
 		virtual void panic() = 0;
 		float param_values[CDevice::MAX_PARAMS];
 	private:
